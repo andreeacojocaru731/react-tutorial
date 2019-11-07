628db2ed0f5f13e2df1df9f528fe0d5af273e2ef
Andreea Cojocaru <andreea.cojocaru@cerner.com>
2019-11-07 14:53:41 +0200

    Updates

---
+++ precache-manifest.26a4fce6a6ef83825df90fea4dca6f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856e58d1eb6d9344f91a4d98b296b23e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f9403415043e5e074f8a",
+    "url": "/react-tutorial/static/css/main.26044dda.chunk.css"
+  },
+  {
+    "revision": "6baeea1694fb64b81d3c",
+    "url": "/react-tutorial/static/js/2.ec35f0bc.chunk.js"
+  },
+  {
+    "revision": "f9403415043e5e074f8a",
+    "url": "/react-tutorial/static/js/main.ec35deb2.chunk.js"
+  },
+  {
+    "revision": "e6a9a83a0a80cd65a374",
+    "url": "/react-tutorial/static/js/runtime-main.dce951c2.js"
+  }
+]);
